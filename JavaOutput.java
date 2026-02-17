5d536c678110e9404da5dce79362aea0b1858e1c
Davidsonnoble <davidsonnoble7gmail.com>
2026-02-17 12:25:41 +0100

    chapter 1

---
+++ JavaOutput.java
@@ -0,0 +1,15 @@
+public class JavaOutput{
+public static void main (String[] args) {
+	System.out.println("Hello world, this is my first java program");
+	System.out.println("Learning Java Is Fun");
+	
+	//Using the print method
+	System.out.print("Do you love \"learning Java\"?");
+	System.out.print(" Yes i love learning Java\n ");
+	
+	
+	//Using the Printf method 
+	System.out.printf("My name is Noble Davidson,\n I am %d Years old %n", 25 );
+}
+
+}	
---
+++ PrimitiveDataType.java
@@ -0,0 +1,33 @@
+public class PrimitiveDataType{
+	public static void main(String[]args){
+		//Whole number primitive data type
+		byte age = 30;
+		System.out.printf("I am %d Years Old", 30);
+		
+		short quantityOfBags = 10000;
+		System.out.printf("The quantity of bags ordered is %d%n",quantityOfBags);
+		
+		
+		int nigeriaPopulation = 2000000000;
+		System.out.printf("The population of Nigeria is %d%n", nigeriaPopulation);
+		
+		long worldPopulation = 9000000000000L;
+		System.out.printf("The world Population is %d%n", worldPopulation);
+		
+		
+		//float-point primitive data type
+		float myBalance = 6945.6000057f;
+		System.out.printf("My account balance is %.2f", myBalance);
+		
+		double cBNBalance = 9874466464.904;
+		System.out.printf("CNB account balanceis %f%n, cBNBalance");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
