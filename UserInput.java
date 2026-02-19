5c9aaa0d13cfef1ceeeeac8d05f5e95b837e4a4c
Davidsonnoble <davidsonnoble7gmail.com>
2026-02-19 12:24:25 +0100

    double quotient = (double)num1/num2;

--- UserInput.java
+++ UserInput.java
@@ -13,6 +13,13 @@ public class UserInput{
 		System.out.print("Enter your age: ");
 		int age = scan.nextInt();
 		scan.nextLine();
+		
+		System.out.print("Enter your gender(Male/Female:");
+		String gender = scan.next();
+		
+		System.out.printf( "Are you happy to learn Java?(True/False: ");
+		boolean isHappy = scan.nextBoolean();
+		
 		System.out.println("");
 		System.out.println("");
 		
@@ -20,6 +27,8 @@ public class UserInput{
 		System.out.printf("Hello %s, How are you today?%n", fullName);
 		System.out.printf("Nice meeting you, you are living in %s%n",address); 
 		System.out.printf("%s You are %d years old",fullName,age);
+		System.out.printf("You are a %s%n ",gender);
+		System.out.printf("Are you happy to learn Java?%b%n ",isHappy);
 	
 	
 	
